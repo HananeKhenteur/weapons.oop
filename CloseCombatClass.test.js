326ae41de9a0f49407734ff3baa952532374f51c
HananeKhenteur <163421584+HananeKhenteur@users.noreply.github.com>
2024-06-14 16:41:14 +0200

    test closeCombatWeapon class

---
+++ CloseCombatClass.test.js
@@ -0,0 +1,26 @@
+const { CloseCombatWeapon } = require('./projet_OOP');
+
+describe('CloseCombatWeapon Class', () =>
+{
+    test('should log attack message with correct damage and range', () =>
+    {
+
+        const consoleLogSpy = jest.spyOn(console, 'log');
+        const katana = new CloseCombatWeapon('Katana', 15, 1);
+
+        katana.attack();
+        expect(consoleLogSpy).toHaveBeenCalledWith(`attacking with Katana at a distance of 1, causing 15 damage.`);
+
+        consoleLogSpy.mockRestore();
+    });
+
+    test('should prepare for attack', () =>
+    {
+        const consoleLogSpy = jest.spyOn(console, 'log');
+        const katana = new CloseCombatWeapon('Katana', 15, 1);
+        katana.prepareForAttack();
+
+        expect(consoleLogSpy).toHaveBeenCalledWith(` sharpen the Katana and attack`);
+        consoleLogSpy.mockRestore();
+    });
+});
