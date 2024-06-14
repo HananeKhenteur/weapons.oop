d941b67d5af10db59220ff86bdbfadaebe70c81d
HananeKhenteur <163421584+HananeKhenteur@users.noreply.github.com>
2024-06-14 14:14:34 +0200

    test weapon class

---
+++ weaponClass.test.js
@@ -0,0 +1,15 @@
+const { Weapon } = require('./projet_OOP');
+
+describe('WeaponClass', () => {
+    let sword;
+    beforeEach(() => {
+        sword = new Weapon('sword', 10, 5);
+    });
+
+    test('should create a weapon with correct properties', () => {
+        expect(sword.name).toBe('sword');
+        expect(sword.damage).toBe(10);
+        expect(sword.range).toBe(5);
+
+    });
+});
