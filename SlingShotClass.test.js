556c79d57a604c62d2fd6a8dd48d82ebb3502eee
HananeKhenteur <163421584+HananeKhenteur@users.noreply.github.com>
2024-06-14 15:01:15 +0200

    test slingshot class

---
+++ SlingShotClass.test.js
@@ -0,0 +1,42 @@
+const { SlingShot } = require('./projet_OOP');
+
+describe('SlingShot Class', () =>
+{
+    let slingShot;
+    beforeEach(() =>
+    {
+        slingShot = new SlingShot('SlingShot', 10, 20);
+    });
+    test('should attack 6 times and check if it needs ammunition', () =>
+    {
+        for (let i = 0; i < 6; i++)
+        {
+            slingShot.attack();
+        }
+
+        expect(slingShot.needsAmmunition()).toBe(true);
+    });
+    test('should attack 6 times and check if it needs ammunition', () =>
+    {
+        for (let i = 0; i < 6; i++)
+        {
+            slingShot.attack();
+        }
+
+        expect(slingShot.needsAmmunition()).toBe(true);
+    });
+    test('should reload when out of stones and attack again', () =>
+    {
+        for (let i = 0; i < 6; i++)
+        {
+            slingShot.attack();
+        }
+        slingShot.prepareForAttack();
+
+        if (slingShot.stones === 0)
+        {
+            slingShot.reload(5);
+        }
+        expect(slingShot.stones).toBe(5);
+    });
+});
