86af43321ebb033a8c8b72caf5ad47d33fb1d678
HananeKhenteur <163421584+HananeKhenteur@users.noreply.github.com>
2024-06-25 23:15:21 +0200

    test bow class using jest

--- BowClass.test.js
+++ BowClass.test.js
@@ -1,59 +1,38 @@
+// tests/BowClass.test.js
 const { Bow } = require('./projet_OOP');
 
-describe('Bow class', () =>
-{
+describe('Bow', () => {
     let bow;
 
-    beforeEach(() =>
-    {
-
+    beforeEach(() => {
         bow = new Bow('Bow', 20, 30, 'High', 3);
     });
 
-    test('should perform attacks and manage ammunition', () =>
-    {
-
-        for (let i = 0; i < 3; i++)
-        {
-            bow.attack();
-        }
-
-        expect(bow.arrows).toBe(0);
-        expect(bow.needsAmmunition()).toBe(true);
-        bow.prepareForAttack();
-
-        expect(bow.arrows).toBe(3);
-        expect(bow.needsAmmunition()).toBe(false);
-
-
+    test('should perform attacks and manage ammunition', () => {
+        console.log = jest.fn();
         bow.attack();
-        expect(bow.arrows).toBe(2);
+        expect(console.log).toHaveBeenCalledWith('Bow shoots an arrow, causing 20 damage. Arrows left: 2');
+        bow.attack();
+        expect(console.log).toHaveBeenCalledWith('Bow shoots an arrow, causing 20 damage. Arrows left: 1');
+        bow.attack();
+        expect(console.log).toHaveBeenCalledWith('Bow shoots an arrow, causing 20 damage. Arrows left: 0');
+        bow.attack();
+        expect(console.log).toHaveBeenCalledWith('Bow is out of arrows, needs more');
     });
 
-    test('should reload correctly when out of arrows', () =>
-    {
-
-        for (let i = 0; i < 3; i++)
-        {
-            bow.attack();
-        }
-        expect(bow.arrows).toBe(0);
-        expect(bow.needsAmmunition()).toBe(true);
-        bow.prepareForAttack();
-
-        expect(bow.arrows).toBe(3);
-        expect(bow.needsAmmunition()).toBe(false);
-    });
-
-    test('should log reloading process correctly', () =>
-    {
-        const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
-
+    test('should reload correctly when out of arrows', () => {
+        bow.arrows = 0;
+        console.log = jest.fn();
         bow.reload();
+        expect(console.log).toHaveBeenCalledWith('Bow is reloaded, arrows are now full at 3');
+    });
 
-        expect(consoleSpy).toHaveBeenCalledWith(`${bow.name} is reloaded, arrows are now full at ${bow.arrows}`);
-        expect(consoleSpy).toHaveBeenCalledWith(`${bow.name} is ready to attack`);
-
-        consoleSpy.mockRestore();
+    test('should log reloading process correctly', () => {
+        bow.arrows = 0;
+        console.log = jest.fn();
+        bow.prepareForAttack();
+        expect(console.log).toHaveBeenCalledWith('Bow needs more arrows');
+        expect(console.log).toHaveBeenCalledWith('Bow is reloaded, arrows are now full at 3');
+        expect(console.log).toHaveBeenCalledWith('Bow is ready to attack');
     });
 });
