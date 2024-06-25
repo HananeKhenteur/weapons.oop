0f034a7d8dfd7a449a57196092b466a2ededbc2d
HananeKhenteur <163421584+HananeKhenteur@users.noreply.github.com>
2024-06-25 23:12:06 +0200

    test weapon class using jest

--- weaponClass.test.js
+++ weaponClass.test.js
@@ -8,8 +8,7 @@ describe('WeaponClass', () =>
         sword = new Weapon('sword', 10, 5);
     });
 
-    test('should create a weapon with correct properties', () =>
-    {
+    test('should create a weapon with correct properties', () => {
         expect(sword.name).toBe('sword');
         expect(sword.damage).toBe(10);
         expect(sword.range).toBe(5);
